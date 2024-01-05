40fe469393ea00a04b5845794d2106ed356a9c39
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-01-05 15:19:06 -0500

    feat: .gitignore file added. main.go: print values in terminal to avoid errors in runtime. tickets.go: initialas functions corrected to avoid errors in runtime.

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -4,10 +4,16 @@ type Ticket struct {
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+func GetTotalTickets(destination string) (int, error) {
+	return 0, nil
+}
 
 // ejemplo 2
-func GetMornings(time string) (int error) {}
+func GetMornings(time string) (int, error) {
+	return 0, nil
+}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+func AverageDestination(destination string, total int) (int, error) {
+	return 0, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
 func main() {
 	total, err := tickets.GetTotalTickets("Brazil")
+	fmt.Println(total, err)
 }
